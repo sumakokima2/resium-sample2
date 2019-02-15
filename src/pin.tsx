8a5d8ad6db1424d218bf2c6fd357045c4f867bb3
sumakokima2 <sumakokima@gmail.com>
2019-02-15 23:59:46 +0100

    コンポーネント化してみました

---
+++ src/pin.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import { Cartesian3 } from "cesium";
+import { Viewer, Entity } from "resium";
+
+export default class Pin extends React.Component<any, any> {
+    constructor(props: any){
+        super(props);
+        this.state = { name: this.props.defaultName };
+    }
+
+    public handleChange(event: any) : void {
+        this.setState({ name: "Charles" });
+    }
+
+    public render() {
+        return (
+            <Entity
+            name= { this.state.name }
+            position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
+            point={{ pixelSize: 10 }}
+            description="hoge"
+          />
+        );
+    }
+}

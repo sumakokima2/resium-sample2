fc41837a73339a849fc89216420abd6749cf6460
sumakokima2 <sumakokima@gmail.com>
2019-02-17 22:26:45 +0100

    コンポーネント化とcsvロード

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { hot } from "react-hot-loader";
-import { Cartesian3 } from "cesium";
-import { Viewer, Entity } from "resium";
+
 import Earth from './earth';
+import loadData from "./data_app";
+
+interface Data {
+  id: string;
+  name: string;
+}
 
 interface State {
+  data:Data[];
 }
 
 class App extends React.PureComponent<{}, State> {
   state = {
+    data: []
   };
+  componentDidMount() {
+    this.loadData();
+    
+  }
 
-  public render() {
-    return <Earth />;
+  async loadData() {
+    const result = await loadData();
+    this.setState({
+      data: result.data,
+    });
+  }
+
+
+  render() {
+    return <Earth pins={this.state.data}/>;
   }
 }
 

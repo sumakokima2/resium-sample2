29d900df38c71f98db6221860e6a8037aa0ddc22
sumakokima2 <sumakokima@gmail.com>
2019-02-17 22:26:15 +0100

    csvデータ読み込み

---
+++ src/data_app.ts
@@ -0,0 +1,21 @@
+
+
+
+export default async function loadData() {
+  const data = await fetch("src/data_app.csv", {
+      credentials: "same-origin",
+    }).then(r => r.text());
+
+  const csv = data
+    .split("\n")
+    .map(d => d.split(",").map(dd => dd.trim()))
+    .filter(d => d.length > 0);
+
+  return {
+    data: csv.map((d, i) => ({
+      id: i.toString(),
+      name: d[1],
+      show:true
+    }))
+  };
+}

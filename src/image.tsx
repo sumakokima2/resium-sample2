d80f24057cd04f96f7f0588ece386d575ad1cb9c
sumakokima2 <sumakokima@gmail.com>
2019-02-27 20:34:41 +0100

    (Cesium as any)

--- src/image.tsx
+++ src/image.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { Cartesian3 } from "cesium";
-import { SingleTileImageryProvider } from "cesium";
+import Cesium from "cesium";
 import { Entity, ImageryLayer } from "resium";
 
-const imageryProvider = new SingleTileImageryProvider({
-    url: "./map01.png",
-    rectangle: Cesium.Rectangle.fromDegrees(31.416667,34.333333, 34.416667,35.333333)
+const imageryProvider = new (Cesium as any).SingleTileImageryProvider({
+    url: "./src/map01.png",
+    rectangle: (Cesium as any).Rectangle.fromDegrees(31.416667,34.333333, 34.416667,35.333333)
   });
 
   export default class Image extends React.Component<any, any> {
